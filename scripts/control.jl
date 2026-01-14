98b57da9a0fe8a65df7dbdd71b3c58a7c6c1880c
Brad Carman <bradleygcarman@outlook.com>
2026-01-13 21:46:58 -0500

    updated control.jl script

--- scripts/control.jl
+++ scripts/control.jl
@@ -7,28 +7,29 @@ import DyadControlSystems as JSC
 using ModelingToolkitParameters
 using WGLMakie
 
+# get parameter defaults
 bot_params = DyadBotComponents.CascadeControlledFlatDyadBotParams()
 
 @component function CascadeControlledFlatDyadBotInput(; name)
     systems = @named begin
-        plant = DyadBotComponents.CascadeControlledFlatDyadBot()
+        bot = DyadBotComponents.CascadeControlledFlatDyadBot()
         position = Blocks.Constant(k=0)
     end
     eqs = [
-        ModelingToolkit.connect(plant.ref, position.output)
+        ModelingToolkit.connect(bot.ref, position.output)
     ]
-    defaults=plant=>bot_params
+    defaults=bot=>bot_params
     System(eqs, t, [], []; systems, name, defaults)
 end
 
-@named bot = CascadeControlledFlatDyadBotInput()
+@named sys = CascadeControlledFlatDyadBotInput()
 spec = JSC.PIDAutotuningAnalysisSpec(;
-    name = :bot,
-    model = bot,
-    measurement = "plant.y",
-    control_input = "plant.u",
-    step_input = "plant.u",
-    step_output = "plant.y",
+    name = :sys,
+    model = sys,
+    measurement = "bot.y",
+    control_input = "bot.u",
+    step_input = "bot.u",
+    step_output = "bot.y",
     Ts = 0.01,           # Sample time
     duration = 25.0,      # Simulation duration
     Ms = 1.5,            # Sensitivity peak constraint
@@ -39,7 +40,7 @@ spec = JSC.PIDAutotuningAnalysisSpec(;
     ki_ub = 0.0,         # Tune PD controller
     num_frequencies = 200,
     soft = true,
-    loop_openings = ["plant.y2", "plant.u2"]
+    loop_openings = ["bot.y2", "bot.u2"]
 )
 
 asol = JSC.run_analysis(spec)
@@ -48,37 +49,38 @@ asol = JSC.run_analysis(spec)
 
 arts = DyadControlSystems.artifacts(asol, :OptimizedParameters)
 
+# update parameters from auto-tune
 bot_params.inner_controller.kp = arts[1, :Kp_standard]
 bot_params.inner_controller.ki = 1/arts[1, :Ti_standard]
 bot_params.inner_controller.kd = arts[1, :Td_standard]
 
 # Simulate Tilted Robot
 bot_params.plant.theta_init = 0.75*π
-sys = mtkcompile(bot)
-prob = ODEProblem(sys, sys.plant => bot_params, (0, 1))
+sys = mtkcompile(sys)
+prob = ODEProblem(sys, sys.bot => bot_params, (0, 1))
 sol = solve(prob)
 
 plot(sol; idxs=sys.plant.plant.theta)
 
 
+# Next Step - Solve Outer Loop ..........
+# @named bot = DyadBotComponents.CascadeControlledFlatDyadBot()
 
-@named bot = DyadBotComponents.CascadeControlledFlatDyadBot()
-
-spec = JSC.PIDAutotuningAnalysisSpec(;
-    name = :bot,
-    model = bot,
-    measurement = "y2",
-    control_input = "u2",
-    step_input = "u2",
-    step_output = "y2",
-    Ts = 0.01,           # Sample time
-    duration = 25.0,      # Simulation duration
-    Ms = 1.5,            # Sensitivity peak constraint
-    Mt = 1.5,            # Complementary sensitivity peak constraint
-    Mks = 400.0,         # Control sensitivity constraint
-    wl = 1e-2,           # Lower frequency bound
-    wu = 1e3,            # Upper frequency bound
-    kd_ub = 0.0,         # Tune PI controller
-    num_frequencies = 200,
-    soft = true
-)
+# spec = JSC.PIDAutotuningAnalysisSpec(;
+#     name = :bot,
+#     model = bot,
+#     measurement = "y2",
+#     control_input = "u2",
+#     step_input = "u2",
+#     step_output = "y2",
+#     Ts = 0.01,           # Sample time
+#     duration = 25.0,      # Simulation duration
+#     Ms = 1.5,            # Sensitivity peak constraint
+#     Mt = 1.5,            # Complementary sensitivity peak constraint
+#     Mks = 400.0,         # Control sensitivity constraint
+#     wl = 1e-2,           # Lower frequency bound
+#     wu = 1e3,            # Upper frequency bound
+#     kd_ub = 0.0,         # Tune PI controller
+#     num_frequencies = 200,
+#     soft = true
+# )
